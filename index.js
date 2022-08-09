7c8323682a18976de269c112ae10cf88e0a3ede2
Christopher Sarmiento <chrisar11@gmail.com>
2022-08-09 14:48:06 -0500

    commit tree 75212b6848563ca93ed7061183471a2d95134e2c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 15.5;
+const num2 = 4;
+const  multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 20;
+const num4 = 5;
+const mod = num3 / num4;
+
+const max = Math.max(5, 10, 15, 20);
